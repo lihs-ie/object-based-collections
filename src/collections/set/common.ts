1d55160231ea2bcd5e4d3b0ebb371966d41c3736
lihs-ie <lihs.dev@gmail.com>
2025-05-08 11:19:18 +0900

    ImmutableSet

---
+++ src/collections/set/common.ts
@@ -0,0 +1,137 @@
+import { type Hasher, HAMTNode, LeafNode, BitmapIndexedNode } from '../hamt';
+import { NullableOptional, Optional } from '../optional';
+
+export interface ImmutableSet<K> {
+  size: () => number;
+  toArray: () => K[];
+  isEmpty: () => boolean;
+  isNotEmpty: () => boolean;
+  add: (key: K) => ImmutableSet<K>;
+  addAll: (...keys: K[]) => ImmutableSet<K>;
+  remove: (key: K) => ImmutableSet<K>;
+  contains: (key: K) => boolean;
+  find: (predicate: (key: K) => boolean) => Optional<K>;
+  map: <R>(mapper: (key: K) => R) => ImmutableSet<R>;
+  filter: (predicate: (key: K) => boolean) => ImmutableSet<K>;
+  forEach: (callback: (key: K) => void) => void;
+  equals: (other: ImmutableSet<K>) => boolean;
+  exists: (predicate: (key: K) => boolean) => boolean;
+}
+
+type Void = undefined;
+const voidValue: Void = undefined;
+
+export const ImmutableSet =
+  (hasher: Hasher) =>
+  <K>(root: HAMTNode<K, Void> | null = null): ImmutableSet<K> => {
+    const toArray = (): K[] => root?.toArray().map(([key]) => key) || [];
+
+    const size = (): number => toArray().length;
+
+    const isEmpty = (): boolean => size() === 0;
+
+    const isNotEmpty = (): boolean => !isEmpty();
+
+    const add = (key: K): ImmutableSet<K> => {
+      const hash = hasher.hash(key);
+
+      if (root === null) {
+        return ImmutableSet(hasher)(LeafNode(hash, key, voidValue));
+      }
+
+      return ImmutableSet(hasher)(root.add(hash, 0, key, voidValue));
+    };
+
+    const addAll = (...keys: K[]): ImmutableSet<K> => {
+      const nextRoot = keys.reduce(
+        (carry, current): HAMTNode<K, Void> =>
+          carry.add(hasher.hash(current), 0, current, voidValue),
+        root || BitmapIndexedNode<K, Void>(),
+      );
+
+      return ImmutableSet(hasher)(nextRoot);
+    };
+
+    const remove = (key: K): ImmutableSet<K> => {
+      const hash = hasher.hash(key);
+
+      if (root === null) {
+        return ImmutableSet(hasher)(null);
+      }
+
+      return ImmutableSet(hasher)(root.remove(hash, 0));
+    };
+
+    const contains = (key: K): boolean => {
+      const hash = hasher.hash(key);
+
+      if (root === null) {
+        return false;
+      }
+
+      return root.contains(hash, 0);
+    };
+
+    const find = (predicate: (key: K) => boolean): Optional<K> => {
+      return NullableOptional(root?.find(predicate)?.key());
+    };
+
+    const map = <R>(mapper: (key: K) => R): ImmutableSet<R> => {
+      const mapped = root?.toArray().map(([key]) => mapper(key)) || [];
+
+      return fromArray(hasher)(mapped);
+    };
+
+    const filter = (predicate: (key: K) => boolean): ImmutableSet<K> => {
+      const filtered = toArray().filter(predicate);
+
+      return fromArray(hasher)(filtered);
+    };
+
+    const forEach = (callback: (key: K) => void): void => {
+      const items = toArray();
+
+      items.forEach((key): void => callback(key));
+    };
+
+    const equals = (comparison: ImmutableSet<K>): boolean => {
+      if (size() !== comparison.size()) {
+        return false;
+      }
+
+      return toArray().every((key): boolean => comparison.contains(key));
+    };
+
+    const exists = (predicate: (key: K) => boolean): boolean => {
+      return root?.exists(predicate) || false;
+    };
+
+    return {
+      size,
+      toArray,
+      isEmpty,
+      isNotEmpty,
+      add,
+      addAll,
+      remove,
+      contains,
+      find,
+      map,
+      filter,
+      forEach,
+      equals,
+      exists,
+    };
+  };
+
+export const fromArray =
+  (hasher: Hasher) =>
+  <T>(keys: T[]): ImmutableSet<T> => {
+    const root = keys.reduce<HAMTNode<T, Void>>(
+      (carry, current): HAMTNode<T, Void> =>
+        carry.add(hasher.hash(current), 0, current, voidValue),
+      BitmapIndexedNode<T, Void>(),
+    );
+
+    return ImmutableSet(hasher)(root);
+  };
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-export const hello = (): void => {
-  console.log('Hello, world!');
-};
+import { ImmutableSet as SetSource, SetFromArray, Hasher } from './collections';
+
+export const ImmutableSet = SetSource(Hasher());
+export type ImmutableSet<T> = SetSource<T>;
+export const createSetFromArray = SetFromArray(Hasher());
+
+export {
+  Optional,
+  NullableOptional,
+  EmptyOptional,
+  ImmutableList,
+} from './collections';
