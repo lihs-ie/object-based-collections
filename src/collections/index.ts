da41912bddc050fabf196175f73f1d61036de7a9
lihs-ie <lihs.dev@gmail.com>
2025-05-09 10:22:49 +0900

    ImmutableMap

---
+++ src/collections/index.ts
@@ -0,0 +1,5 @@
+export { Optional, NullableOptional, EmptyOptional } from './optional';
+export { Hasher } from './hamt';
+export { ImmutableList } from './list';
+export { ImmutableSet, SetFromArray } from './set';
+export { ImmutableMap, MapFromArray, MapFromObject } from './map';
